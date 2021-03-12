8fe21824d7e22b902679fa174f34eac80237472d
york <york11122@gmail.com>
2021-03-12 22:17:40 +0800

    123
    123

--- src/core/socket/socket.gateway.ts
+++ src/core/socket/socket.gateway.ts
@@ -1,30 +1,35 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayConnection } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+} from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { verifyToken } from '@/utils/auth/jwt'
-import { UseGuards } from '@nestjs/common'
-import { WsAuthGuard } from '@/common/guards/ws-auth.guard'
-@WebSocketGateway(81, { origins: "*:*", namespace: "notification" })
+import { verifyToken } from '@/utils/auth/jwt';
+import { UseGuards } from '@nestjs/common';
+import { WsAuthGuard } from '@/common/guards/ws-auth.guard';
+@WebSocketGateway(8005, { origins: '*:*', namespace: 'notification' })
 export class SocketGateway implements OnGatewayConnection {
-  constructor() { }
-  @WebSocketServer() ws: Server
+  constructor() {}
+  @WebSocketServer() ws: Server;
 
   @UseGuards(WsAuthGuard)
   @SubscribeMessage('message')
-  handleMessage (client: any, payload: any): string {
+  handleMessage(client: any, payload: any): string {
     return 'Hello world!';
   }
 
-  async handleConnection (client: Socket, ...args: any[]) {
-    const token = client.handshake.query.token
-    const [err, data] = await verifyToken(token, 'accessToken')
+  async handleConnection(client: Socket, ...args: any[]) {
+    const token = client.handshake.query.token;
+    const [err, data] = await verifyToken(token, 'accessToken');
     if (err) {
       client.disconnect(true);
-      return
+      return;
     }
-    client.join(data._id)
+    client.join(data._id);
   }
 
-  sendToAll (msg: string, userId: string) {
+  sendToAll(msg: string, userId: string) {
     this.ws.to(userId).emit('message', msg);
   }
 }
